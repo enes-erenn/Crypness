710ec078bcf6c5f521cae7a3816078dcfd809a87
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-03 21:30:08 +0300

    Added Searching Feature and changed Listing Feature

--- background.js
+++ background.js
@@ -33,3 +33,12 @@ function getFavPrices() {
     });
 }
 setInterval(getFavPrices, 3600000);
+
+/* 
+function handleMessage(request, sender, sendResponse) {
+  console.log("Message from the content script: " + request.greeting);
+  sendResponse({ response: "Response from background script" });
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
+ */
--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@ const closeModal = document.querySelector(".close-modal");
 const currensies = document.getElementById("currensies");
 const currencyBtn = document.querySelectorAll(".currency");
 const hours = document.querySelectorAll(".hour");
+const notifications = document.getElementById("notifications");
 let count = document.getElementById("count");
 let result;
 let currency = localStorage.getItem("curr")
@@ -17,7 +18,7 @@ let currHour = localStorage.getItem("hour")
   ? localStorage.getItem("hour")
   : "24h";
 
-chrome.action.setIcon({ path: "assets/images/icon128.png" });
+// chrome.action.setIcon({ path: "assets/images/icon128.png" });
 
 document.getElementById(`${currency}`).style.border = "1px solid black";
 document.getElementById(`${currHour}`).style.border = "1px solid black";
@@ -108,38 +109,56 @@ function setCurr(c) {
   }
 }
 
+let coins = [];
+const coinCardTemplate = document.querySelector("[data-coin-template]");
+const coinCardContainer = document.querySelector("[data-coin-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  console.log(value);
+  coins.forEach((coin) => {
+    const isVisible = coin.name.includes(value) || coin.symbol.includes(value);
+    coin.element.classList.toggle("hidden", !isVisible);
+  });
+});
+
 fetch(
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h,24h`
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h,24h"
 )
   .then((res) => res.json())
-  .then((coins) => {
-    result = coins.map(
-      (coin) =>
-        `
-          <div class="coin" >
-            <div class="coin_header">
-              <img class="image" src=${coin.image} loading="lazy" />
-              <h3 class="coin_name" >${coin.name}</h3>
-            </div>
-            <div>
-              <h4 class="headers" >Current Value</h4>
-              <span>${currSign} ${coin.current_price}</span>
-            </div>
-            <div>
-              <h4 class="headers">${currHour} Change</h4>
-              <span>${
-                currHour === "24h"
-                  ? coin.price_change_percentage_24h_in_currency.toFixed(2)
-                  : coin.price_change_percentage_1h_in_currency.toFixed(2)
-              }%</span>
-            </div>
-          </div>
-        `
-    );
-    loader.classList.add("hidden");
-    coinList.style.display = "grid";
-    coinList.innerHTML = result.join("");
-    count.innerText = `Listed ${coinList.childElementCount} cryptocurrencies`;
+  .then((data) => {
+    coins = data.map((coin) => {
+      const card = coinCardTemplate.content.cloneNode(true).children[0];
+      const name = card.querySelector("[data-name");
+      const price = card.querySelector("[data-price]");
+      const image = card.querySelector("[data-image]");
+      const change = card.querySelector("[data-change]");
+      const changeHeader = card.querySelector("[data-change-header]");
+      name.textContent = coin.name;
+      price.textContent = `${currSign}${coin.current_price} `;
+      image.src = coin.image;
+      change.textContent = `${
+        currHour === "24h"
+          ? coin.price_change_percentage_24h_in_currency.toFixed(2)
+          : coin.price_change_percentage_1h_in_currency.toFixed(2)
+      }%`;
+      changeHeader.textContent = `${currHour} Change`;
+      coinCardContainer.append(card);
+      loader.classList.add("hidden");
+
+      count.innerText = `Listed ${
+        coinList.childElementCount - 1
+      } cryptocurrencies`;
+      return {
+        image: coin.image,
+        name: coin.id,
+        price: coin.current_price,
+        symbol: coin.symbol,
+        change: coin.price_change_percentage_24h_in_currency.toFixed(2),
+        element: card,
+      };
+    });
   });
 
 settings.addEventListener("click", () => {
@@ -151,3 +170,26 @@ closeModal.addEventListener("click", () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 });
+
+/* notifications.addEventListener("click", () => {}); */
+/* function handleResponse(message) {
+  console.log(`Message from the background script:  ${message.response}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function notifyBackgroundPage(e) {
+  var sending = chrome.runtime.sendMessage({
+    greeting: "Greeting from the content script",
+  });
+  sending.then(handleResponse, handleError);
+}
+
+window.addEventListener("click", notifyBackgroundPage); */
+/* 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(input.value);
+}); */
